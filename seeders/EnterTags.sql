d07dc5ac4e810a2bffd52782942fc71ef4527e4b
timmyreilly <timreilly@live.com>
2019-02-08 14:25:08 -0800

    add tags

---
+++ seeders/EnterTags.sql
@@ -0,0 +1,8 @@
+/**Tags**/
+delete from dbo.tags;
+insert into dbo.tags (name, createdAt, updatedAt) values ('Desk', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET());
+insert into dbo.tags (name, createdAt, updatedAt) values ('Kitchen', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET());
+insert into dbo.tags (name, createdAt, updatedAt) values ('Bathroom', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET());
+insert into dbo.tags (name, createdAt, updatedAt) values ('Printer', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET());
+insert into dbo.tags (name, createdAt, updatedAt) values ('Security', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET());
+insert into dbo.tags (name, createdAt, updatedAt) values ('Other', SYSDATETIMEOFFSET(), SYSDATETIMEOFFSET());
